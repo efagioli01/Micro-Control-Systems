5f50987d1dae5abd787cde58f4b3fc8963c46b7c
Erica Fagioli <efagioli01@gmail.com>
2021-10-17 20:02:33 -0400

    removed extra code

--- app.js
+++ app.js
@@ -12,33 +12,24 @@ buttons.forEach((btn) => {
 });
 
 
-// function increment() {
-//     document.getElementById('demoInput').stepUp();
-// }
-// function decrement() {
-//     document.getElementById('demoInput').stepDown();
-// }
-
-
 
 function getVal() {
     const val = document.querySelector('input').value;
     console.log(val);
     
 
+    if (val < 20) {
+        
+        document.getElementById("firstInput").removeAttribute('disabled', 'disabled');
+        document.getElementById("dis").removeAttribute('disabled', 'disabled');
+        document.getElementById("bt").removeAttribute('disabled', 'disabled');
 
-    // if (val < 20) {
-    //     console.log('yes')
-    //     document.getElementById("firstInput").removeAttribute('disabled', 'disabled');
-    //     document.getElementById("dis").removeAttribute('disabled', 'disabled');
-    //     document.getElementById("bt").removeAttribute('disabled', 'disabled');
-
-    // } else {
-    //     console.log('no')
-    //     document.getElementById("firstInput").setAttribute('disabled', 'disabled');
-    //     document.getElementById("dis").setAttribute('disabled', 'disabled');
-    //     document.getElementById("bt").setAttribute('disabled', 'disabled');
-    // }
+    } else {
+        
+        document.getElementById("firstInput").setAttribute('disabled', 'disabled');
+        document.getElementById("dis").setAttribute('disabled', 'disabled');
+        document.getElementById("bt").setAttribute('disabled', 'disabled');
+    }
 }
 
 
